0ff341a91f037f28e3b5348b7731d305eae7dd54
Sagar Behara <sagar@cryptocontrol.io>
2018-08-23 17:25:48 +0530

    Basic example for C library

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World!");
+            stringUtility.StringLibrary clsString = new stringUtility.StringLibrary ();
+            Console.WriteLine(clsString.StartsWithUpper("kts"));
+            Console.WriteLine(clsString.IsStringPalindrome("12321"));
+        }
+    }
+}
